00f24ce2bfc5b3b7dbbf05f6fb67a55b3fa5fb67
gizeasy <gizeasy@gmail.com>
2020-03-18 11:17:13 +0300

    *

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: {
+    autoprefixer: {},
+    'postcss-import': {},
+    'postcss-simple-vars': {},
+    'postcss-color-function': {},
+    // 'postcss-calc': {},
+    'postcss-nested': {},
+  },
+};
